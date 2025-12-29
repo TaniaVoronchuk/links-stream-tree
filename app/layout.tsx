4cb1783ba066c2078a239c57ca841d7a5a111713
TaniaVoronchuk <tetiana.voronchuk@gmail.com>
2025-12-29 02:31:09 +0200

    add openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,30 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Links-Stream Magic Tree 🎄",
   description: "Create your own Christmas magic with Links-Stream",
+
+  openGraph: {
+    title: "Links-Stream Magic Tree 🎄",
+    description: "Create your own Christmas magic with Links-Stream",
+    url: "https://links-stream-tree.vercel.app/", 
+    siteName: "Links-Stream",
+    locale: "en",
+    type: "website",
+    images: [
+      {
+        url: "/start-the-magic.png", 
+        width: 1200,
+        height: 630,
+        alt: "Links-Stream Christmas Tree Preview",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Links-Stream Magic Tree 🎄",
+    description: "Create your own Christmas magic with Links-Stream",
+    images: ["/start-the-magic.png"], 
+  },
 };
 
 export default function RootLayout({
